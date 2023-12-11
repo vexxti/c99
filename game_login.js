caf08c65be5401d09ce15bd79357c0243ef7c494
vexxti <141842267+vexxti@users.noreply.github.com>
2023-12-11 20:02:29 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,13 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+    player1_name = localStorage.setItem();
+    player2_name = localStorage.setItem();
+
+    window.location = "game_page.html";
+}
+
